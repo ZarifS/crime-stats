34197b448abc1013f3e4045cf1e37e69da8e5f60
ZarifS <zarif.shahriar@gmail.com>
2020-02-20 18:22:11 -0500

    Update to vancouver-fact-table

--- scripts/vancouver_fact_table.py
+++ scripts/vancouver_fact_table.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[13]:
 
 
 import pandas as pd
@@ -9,11 +9,11 @@ vancouver_data = pd.read_csv("./vancouver-parent_data.csv")
 print(vancouver_data.shape[0])
 
 
-# In[2]:
+# In[14]:
 
 
-vancouver_date_data = pd.read_csv("./vancouver-date.csv")
-print(vancouver_date_data.shape[0])
+merged_date_data = pd.read_csv("../merged-datasets/merged-dates.csv")
+print(merged_date_data.shape[0])
 
 vancouver_address_data = pd.read_csv("./vancouver-address.csv")
 print(vancouver_address_data.shape[0])
@@ -25,80 +25,90 @@ print(vancouver_crime_data.shape[0])
 # vancouver_date_data.head()
 
 
-# In[3]:
+# In[15]:
 
 
 vancouver_data.head(5)
 
 
-# In[4]:
+# In[16]:
 
 
-vancouver_date_data.head(5)
+merged_date_data.head(5)
 
 
-# In[5]:
+# In[18]:
 
 
-#SAMPLE TEST
-# sample_van_data = vancouver_data.iloc[0:5]
-# sample_date_data = vancouver_date_data.iloc[0:5]
-# new_van_data_sample = pd.merge(sample_van_data, sample_date_data, how='left', left_on=['YEAR','MONTH', 'DAY'], right_on = ['year','month', 'day'])
-# new_van_data_sample.head()
+vancouver_address_data.head(5)
 
 
-# In[6]:
+# In[19]:
+
+
+vancouver_crime_data.head(5)
+
+
+# In[21]:
 
 
 #MERGING VAN_DATA W/ VAN_DATE_DATA
-new_van_data = pd.merge(vancouver_data, vancouver_date_data, how='left', left_on=['YEAR','MONTH', 'DAY'], right_on = ['year','month', 'day'])
+new_van_data = pd.merge(vancouver_data, merged_date_data, how='left', left_on=['HUNDRED_BLOCK'], right_on = ['location_name'])
 
 
-# In[7]:
+# In[23]:
 
 
-#TEST
-# new_van_data.loc[(new_van_data['YEAR'] == 2019) & (new_van_data['MONTH'] == 5) & (new_van_data['DAY'] == 5)]
+new_van_data.head(5)
 
 
-# In[8]:
+# In[24]:
 
 
-new_van_data.head()
-
+# Removing all the other merged-date columns and only keeping location_key
 new_van_data = new_van_data.iloc[:, :11]
 new_van_data.head()
 
 
-# In[9]:
+# In[26]:
 
 
-#Adding Address Key
-new_van_data = pd.concat([new_van_data, vancouver_address_data], axis=1)
-new_van_data.head()
+#Adding Date Key
+new_van_data = pd.merge(new_van_data, merged_date_data, how='left', left_on=['YEAR','MONTH', 'DAY'], right_on = ['year','month', 'day'])
+new_van_data.head(5)
 
 
-# In[10]:
+# In[28]:
 
 
+# Getting rid of extra date attribute
 new_van_data = new_van_data.iloc[:, :12]
 new_van_data.head()
 
 
-# In[11]:
+# In[29]:
 
 
+# Confirm size of dataset is still the same as crime-dataset
+new_van_data.shape[0]
+
+
+# In[30]:
+
+
+# Since the order of the crime-dataset and original dataset are the same, we can just concat the crime-dim info
 new_van_data = pd.concat([new_van_data, vancouver_crime_data], axis=1)
 
 
-# In[12]:
+# In[31]:
 
 
+# Remove extra crime info
 new_van_data = new_van_data.iloc[:, :13]
 new_van_data.head()
 
 
-# In[13]:
+# In[32]:
 
 
 #Adding the is_nighttime column
@@ -114,14 +124,14 @@ new_van_data['is_nighttime'] = new_van_data.apply (lambda row: is_nighttime(row)
 new_van_data.head()
 
 
-# In[14]:
+# In[33]:
 
 
 # Output the types of crimes in dataset
 new_van_data.TYPE.unique()
 
 
-# In[15]:
+# In[34]:
 
 
 # Add a new column for is_traffic and is_fatal
@@ -139,13 +149,13 @@ new_van_data['is_fatal'] = is_fatal
 new_van_data['is_traffic'] = is_traffic
 
 
-# In[16]:
+# In[35]:
 
 
 new_van_data.head(5)
 
 
-# In[17]:
+# In[36]:
 
 
 # Drop all uneeded columns
@@ -153,8 +163,15 @@ new_van_data = new_van_data.drop(['YEAR', 'MONTH', 'DAY', 'HOUR', 'MINUTE', 'HUN
 new_van_data.head(5)
 
 
-# In[19]:
+# In[37]:
 
 
+# Export to CSV
 new_van_data.to_csv('./vancouver_fact_table.csv', index=False)
+
+
+# In[ ]:
+
+
+# End
 
