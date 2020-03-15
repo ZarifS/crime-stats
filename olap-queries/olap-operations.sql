ac110d40123e3f90d180688848edc98fac8a696f
ChukwuemekaAjaero <emeka.ajaero7@gmail.com>
2020-03-15 13:50:34 -0400

    Simple Custom Query

--- olap-queries/olap-operations.sql
+++ olap-queries/olap-operations.sql
@@ -7,3 +7,14 @@
 -- Dice (Zarif)
 
 -- Combined Operations (Zaid)
+
+SELECT "Custom SQL Query"."neighborhood" AS "neighborhood",
+  SUM("Custom SQL Query"."count") AS "sum:count:ok"
+FROM (
+  SELECT I.neighborhood, count(*)
+  FROM t_location_dim as I
+  INNER JOIN t_fact_table as a
+  ON I.location_key = a.location_key
+  GROUP BY neighborhood
+) "Custom SQL Query"
+GROUP BY 1
