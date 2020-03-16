f18f079877f1460ff04c7d92089d49aca4d847c9
ZarifS <zarif.shahriar@gmail.com>
2020-03-16 15:58:37 -0400

    Added slice query 2 (total crimes per location)

--- olap-queries/olap-operations.sql
+++ olap-queries/olap-operations.sql
@@ -1,6 +1,4 @@
--- Drill Down (Emeka)
-
--- Number of types of crimes per city -> neighbourhood -> location
+-- Drill Down Query 1 - Number of types of crimes per city -> neighbourhood -> location
 SELECT "Custom SQL Query"."city" AS "city",
   "Custom SQL Query"."crime_type" AS "crime_type",
   "Custom SQL Query"."location_name" AS "location_name",
@@ -19,8 +17,7 @@ GROUP BY 1,
   3,
   4;
 
--- Number of crimes per location(city -> neighbourhood -> location name) per date (year -> month -> day)
-
+-- Drill Down Query 2 - Number of crimes per location(city -> neighbourhood -> location name) per date (year -> month -> day)
 SELECT "Location"."city" AS "city",
   "Location"."day" AS "day",
   "Location"."location_name" AS "location_name",
@@ -43,12 +40,7 @@ GROUP BY 1,
   5,
   7;
 
--- Roll Up (Emeka)
-
-/*
-Number of crimes per city per year
-*/
-
+-- Roll Up Query 1 - Number of crimes per city per year
 SELECT "Location"."city" AS "city",
   SUM(1) AS "sum:Number of Records:ok",
   "Location"."year" AS "year"
@@ -63,10 +55,7 @@ FROM (
 GROUP BY 1,
   3;
 
-/*
-Number of crimes that occurr on a holiday per year per city (includes holiday name as well)
-*/
-
+-- Roll Up Query 2 - Number of crimes that occurr on a holiday per year per city (includes holiday name as well)
 SELECT
   "Date_AND_Fact"."american_holiday_name" AS "american_holiday_name",
   "Date_AND_Fact"."canadian_holiday_name" AS "canadian_holiday_name",
@@ -87,12 +76,7 @@ GROUP BY 1,
   3,
   5;
 
-
-
-
--- Slice (Zarif)
-
-/* Number of crimes that occurr on March 2016*/
+-- Slice Query 1 - Number of crimes that occurr on March 2016
 SELECT "Date_AND_Fact"."city" AS "city",
   SUM(1) AS "number_of_crimes"
 FROM (
@@ -106,14 +90,16 @@ FROM (
 WHERE (("Date_AND_Fact"."month" = '3') AND ("Date_AND_Fact"."year" = '2016'))
 GROUP BY 1
 
+-- Slice Query 2 - Number of crimes that occurr per location
+select l.neighborhood, l.city, d.year, SUM(1) AS "number_of_crimes" 
+from t_fact_table as f
+inner join t_date_dim as d
+on f.date_key = d.date_key
+inner join t_location_dim as l
+on f.location_key = l.location_key
+group by (l.neighborhood, l.city, d.year)
 
--- Dice (Zarif)
-
-/*
-Number of crimes per location (city -> neighbourhood -> locaiton name) per year
-in January, February and March
-*/
-
+-- Dice Query 1 - Number of crimes per location (city -> neighbourhood -> locaiton name) per year in January, February and March
 SELECT "Location"."city" AS "city",
   "Location"."day" AS "day",
   "Location"."location_name" AS "location_name",
@@ -137,5 +123,5 @@ GROUP BY 1,
   5,
   7
 
--- Combined Operations (Zaid)
+-- Combined Operations
 
