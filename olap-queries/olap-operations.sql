66bb8b1e46d033650c9b35eb508deed37d24186e
Zaid Saeed <zsaee060@uottawa.ca>
2020-03-16 17:21:43 -0400

    added roll up query

--- olap-queries/olap-operations.sql
+++ olap-queries/olap-operations.sql
@@ -63,6 +63,32 @@ FROM (
 GROUP BY 1,
   3;
 
+/*
+Number of crimes that occurr on a holiday per year per city (includes holiday name as well)
+*/
+
+SELECT
+  "Date_AND_Fact"."american_holiday_name" AS "american_holiday_name",
+  "Date_AND_Fact"."canadian_holiday_name" AS "canadian_holiday_name",
+  "Date_AND_Fact"."city" AS "city",
+  SUM(1) AS "number_of_crimes",
+  "Date_AND_Fact"."year" AS "year"
+FROM (
+  select * from
+  t_fact_table
+  inner join t_date_dim
+  on t_fact_table.date_key = t_date_dim.date_key
+  inner join t_location_dim
+  on t_fact_table.location_key = t_location_dim.location_key
+) "Date_AND_Fact"
+WHERE "Date_AND_Fact"."is_american_holiday"
+GROUP BY 1,
+  2,
+  3,
+  5;
+
+
+
 
 -- Slice (Zarif)
 
