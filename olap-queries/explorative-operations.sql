8c0299aa64dc1b9e041e0681fcf9f5bf1295e77c
Zaid Saeed <zsaee060@uottawa.ca>
2020-03-18 13:13:19 -0400

    added first window query, returns many duplicates though (has bugg)

--- olap-queries/explorative-operations.sql
+++ olap-queries/explorative-operations.sql
@@ -78,3 +78,21 @@ GROUP BY 1,
   6
 
 -- Utilizing Windowing Clause (Zaid)
+-- HAS BUG IN IT!! Returns alot of duplicates
+-- compares # of crimes per neighborhood from month to month 
+-- uses the window clause 
+SELECT 
+   COUNT(*) over w,
+   month,
+   neighborhood,
+   year
+FROM (
+  select distinct * from
+  t_fact_table
+  full outer join t_location_dim
+  on t_fact_table.location_key = t_location_dim.location_key
+  full outer join t_date_dim
+  on t_fact_table.date_key = t_date_dim.date_key
+) "Location"
+WINDOW w AS (PARTITION BY neighborhood order by month, year)
+limit 1000
